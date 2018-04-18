6098be6d830d2d9e9517ad89763ae4e1fc277e4c
James Ross <jross.nyc111@gmail.com>
2018-04-18 18:16:20 -0400

    trips

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'trips/index'
+
+  get 'trips/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'items#index'
   resources :items
